978f70332a92ef6d311d5922377c817e2a160bd0
dagfinndybvig <dagfinn.dybvig@gmail.com>
2026-03-19 15:18:06 +0100

    Remove debug code that was causing build issues, keeping the simplified 2-button name selection system

--- docs/index.js
+++ docs/index.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmprwkzois_.js
+// include: /tmp/tmpz4ipdqtb.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -203,21 +203,21 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmprwkzois_.js
-// include: /tmp/tmpkzw29j43.js
+// end include: /tmp/tmpz4ipdqtb.js
+// include: /tmp/tmpkt9_jx3c.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpkzw29j43.js
-// include: /tmp/tmp6up6p8rn.js
+  // end include: /tmp/tmpkt9_jx3c.js
+// include: /tmp/tmpoe1esiy6.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp6up6p8rn.js
+  // end include: /tmp/tmpoe1esiy6.js
 
 
 var arguments_ = [];
